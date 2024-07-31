b9b036d86b9cd17075cc49fedede731fe3b087aa
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-07-31 08:08:41 -0500

    update sg for rds

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -11,8 +11,7 @@ resource "aws_db_instance" "main" {
   parameter_group_name = "default.postgres13"
   skip_final_snapshot  = true
 
-  vpc_security_group_ids = [aws_security_group.db.id]
-  db_subnet_group_name   = aws_db_subnet_group.main.name
+  db_subnet_group_name = aws_db_subnet_group.main.name
 }
 
 resource "aws_db_subnet_group" "main" {
