56f54467b0269b5a058deef98d303640b8aea621
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-07-31 00:13:05 -0500

    enable ecs

--- infra/modules/ecs/outputs.tf
+++ infra/modules/ecs/outputs.tf
@@ -2,6 +2,6 @@ output "cluster_arn" {
   value = aws_ecs_cluster.main.arn
 }
 
-output "service_name" {
-  value = aws_ecs_service.app.name
-}
+# output "service_name" {
+#   value = aws_ecs_service.app.name
+# }
