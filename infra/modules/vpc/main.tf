1362b0b7fb4cfccb0f1a3ff78f36eecdc2c935a6
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-07-30 23:56:59 -0500

    update subnets

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -8,26 +8,14 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public" {
-  count             = length(data.aws_availability_zones.available)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
-  availability_zone = data.aws_availability_zones.available[count.index]
-
-  tags = {
-    Name = "${var.env}-public-subnet-${count.index + 1}"
-  }
-}
-
+# create 2 private subnets
 resource "aws_subnet" "private" {
-  count             = length(data.aws_availability_zones.available)
+  count             = 2
   vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + length(data.aws_availability_zones.available))
-  availability_zone = data.aws_availability_zones.available[count.index]
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index + 2)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
   tags = {
-    Name = "${var.env}-private-subnet-${count.index + 1}"
+    Name = "private-subnet-${count.index + 1}"
   }
 }
-
-# Add other VPC components like Internet Gateway, NAT Gateway, Route Tables, etc.
