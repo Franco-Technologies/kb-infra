108d6a541b36a450d972b43adaa3c7e447495d2f
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-07-31 08:19:10 -0500

    update outputs

--- infra/modules/api_gateway/outputs.tf
+++ infra/modules/api_gateway/outputs.tf
@@ -8,12 +8,12 @@ output "root_resource_id" {
   value       = aws_api_gateway_resource.root.id
 }
 
-output "deployment_id" {
-  description = "The ID of the deployment"
-  value       = aws_api_gateway_deployment.this.id
-}
+# output "deployment_id" {
+#   description = "The ID of the deployment"
+#   value       = aws_api_gateway_deployment.this.id
+# }
 
-output "vpc_link_id" {
-  description = "The ID of the VPC Link"
-  value       = aws_api_gateway_vpc_link.this.id
-}
+# output "vpc_link_id" {
+#   description = "The ID of the VPC Link"
+#   value       = aws_api_gateway_vpc_link.this.id
+# }
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,16 +2,14 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-# output "ecs_cluster_arn" {
-#   value = module.ecs.cluster_arn
-# }
+output "ecs_cluster_arn" {
+  value = module.ecs.cluster_arn
+}
 
-# output "rds_endpoint" {
-#   value = module.rds.db_endpoint
-# }
+output "rds_endpoint" {
+  value = module.rds.db_endpoint
+}
 
-# output "s3_bucket_name" {
-#   value = module.s3.bucket_name
-# }
-
-# Add other relevant outputs
+output "api_gateway_rest_api_id" {
+  value = module.api_gateway.rest_api_id
+}
