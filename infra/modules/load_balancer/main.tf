98a17a0a27cdd12c5a57be6c0cf59d980709c80d
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-07-31 18:36:35 -0500

    update type

--- infra/modules/load_balancer/main.tf
+++ infra/modules/load_balancer/main.tf
@@ -22,8 +22,9 @@ resource "aws_lb_listener" "main" {
 
 # target group resource
 resource "aws_lb_target_group" "main" {
-  name     = "${var.env}-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name        = "${var.env}-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
 }
