6fe36e26d32d9007e750ae7ccc125c3ef054b88e
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-07-31 18:27:56 -0500

    udpate lb

--- infra/modules/load_balancer/main.tf
+++ infra/modules/load_balancer/main.tf
@@ -26,14 +26,4 @@ resource "aws_lb_target_group" "main" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    port                = "traffic-port"
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
-    interval            = 10
-  }
 }
