e4904705b5fb05db701e13c4439a48982aedde2c
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-07-31 13:46:33 -0500

    remove env var

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,11 +3,6 @@ variable "aws_region" {
   type        = string
 }
 
-variable "env" {
-  description = "Environment name (dev, staging, prod)"
-  type        = string
-}
-
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
