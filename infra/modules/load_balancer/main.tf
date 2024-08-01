e37fa10b39fe5d11b23791a045a8b54e8fe71487
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-07-31 19:47:35 -0500

    destroy listerner

--- infra/modules/load_balancer/main.tf
+++ infra/modules/load_balancer/main.tf
@@ -9,16 +9,16 @@ resource "aws_lb" "main" {
 }
 
 # listener resource
-resource "aws_lb_listener" "main" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = 80
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "main" {
+#   load_balancer_arn = aws_lb.main.arn
+#   port              = 80
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.main.arn
+#   }
+# }
 
 # target group resource
 resource "aws_lb_target_group" "main" {
