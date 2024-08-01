6ba6f17afb9119d5b5bbb28c1edb236e1967aa78
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-08-01 18:34:12 -0500

    add http to lb sg

--- infra/modules/load_balancer/main.tf
+++ infra/modules/load_balancer/main.tf
@@ -21,9 +21,10 @@ resource "aws_security_group" "lb_sg" {
   }
 
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
