ee4ee2f5d45e8a3f9f4c3e3c1748504af396cd11
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-07-31 22:23:16 -0500

    add rt

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -53,3 +53,25 @@ resource "aws_internet_gateway" "main" {
     Name = "${var.env}-tenant-management-igw"
   }
 }
+
+# route table for the public subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+
+}
+
+# associate the public subnets with the route table
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+}
