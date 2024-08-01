1f39c28f0dd2f18bf8d4962e5b57db7c99da1f51
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-08-01 15:06:35 -0500

    update endpoints

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -102,17 +102,19 @@ resource "aws_route_table_association" "private" {
 }
 
 resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id             = aws_vpc.main.id
-  service_name       = "com.amazonaws.us-east-2.ecr.dkr"
-  vpc_endpoint_type  = "Interface"
-  security_group_ids = [aws_security_group.default.id]
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.us-east-2.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.default.id]
+  private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id             = aws_vpc.main.id
-  service_name       = "com.amazonaws.us-east-2.ecr.api"
-  vpc_endpoint_type  = "Interface"
-  security_group_ids = [aws_security_group.default.id]
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.us-east-2.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.default.id]
+  private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "s3" {
@@ -122,8 +124,9 @@ resource "aws_vpc_endpoint" "s3" {
 }
 
 resource "aws_vpc_endpoint" "logs" {
-  vpc_id             = aws_vpc.main.id
-  service_name       = "com.amazonaws.us-east-2.logs"
-  vpc_endpoint_type  = "Interface"
-  security_group_ids = [aws_security_group.default.id]
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.us-east-2.logs"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.default.id]
+  private_dns_enabled = true
 }
