958e4c191a98b4a13807641767a3673440683634
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-08-05 14:17:05 -0500

    update handler

--- infra/modules/lambda/src/handler.py
+++ infra/modules/lambda/src/handler.py
@@ -57,7 +57,7 @@ def validate_jwt_token(token, trusted_issuers):
     signing_key = jwks_client.get_signing_key_from_jwt(token)
 
     try:
-        payload = jwt.decode(token, signing_key.key, algorithms=['RS256'], audience=issuer, issuer=issuer)
+        payload = jwt.decode(token, signing_key.key, algorithms=['RS256'], issuer=issuer)
         return payload
     except jwt.ExpiredSignatureError:
         print("Token has expired")
