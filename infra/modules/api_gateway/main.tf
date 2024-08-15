1ca95b39893811aec28e2b9b9b8a7741af87f7c8
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-08-15 00:07:53 -0500

    update deployment

--- infra/modules/api_gateway/main.tf
+++ infra/modules/api_gateway/main.tf
@@ -37,6 +37,22 @@ resource "aws_api_gateway_method" "this" {
   authorization = "NONE"
 }
 
+# Integration
+resource "aws_api_gateway_integration" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.this.http_method
+
+  type = "MOCK"
+
+  request_templates = {
+    "application/json" = jsonencode({
+      statusCode = 200
+    })
+  }
+
+}
+
 # deployment
 resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
--- infra/modules/api_gateway/outputs.tf
+++ infra/modules/api_gateway/outputs.tf
@@ -8,5 +8,4 @@ output "api_gateway_url" {
 }
 
 output "root_resource_id" {
-  value = aws_api_gateway_rest_api.this.root_resource_id
-}
+  value = aws_api_gateway_resource.root.id
