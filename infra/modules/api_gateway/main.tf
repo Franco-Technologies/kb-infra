ac0740e084250235dc1b171f9112a1964b02424a
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-10-18 09:30:01 -0500

    Refactor API Gateway response handling

--- infra/modules/api_gateway/main.tf
+++ infra/modules/api_gateway/main.tf
@@ -37,6 +37,28 @@ resource "aws_api_gateway_method" "this" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_method_response" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.this.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.this.http_method
+  status_code = aws_api_gateway_method_response.this.status_code
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
 # Integration
 resource "aws_api_gateway_integration" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
